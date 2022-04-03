0ccbc7c8e591f375a3f8006a1d7823ed2fa1e7ed
Froilan Quiseo Sulit <froilansulit017@gmail.com>
2022-04-03 18:26:00 +0800

    Create Java End-of-file.java

---
+++ HackerRank-9/Java End-of-file.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    
+    public static void main(String[] args) {
+        Scanner ww = new Scanner(System.in);
+        
+        for(int i = 1; ww.hasNext(); ++i) {
+            System.out.println(i + " " + ww.nextLine());
+        }
+    }
+}
